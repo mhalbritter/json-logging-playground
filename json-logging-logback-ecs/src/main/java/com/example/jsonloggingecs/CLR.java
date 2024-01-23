2ee007050a2a7b42dbce4f27ecb2a93eecc3f7f1
Moritz Halbritter <moritz.halbritter@broadcom.com>
2024-01-23 13:28:36 +0100

    ECS MDC test

--- json-logging-logback-ecs/src/main/java/com/example/jsonloggingecs/CLR.java
+++ json-logging-logback-ecs/src/main/java/com/example/jsonloggingecs/CLR.java
@@ -2,6 +2,7 @@ package com.example.jsonloggingecs;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -14,5 +15,8 @@ public class CLR implements CommandLineRunner {
         LOGGER.info("Run run run");
         LOGGER.error("Boom", new RuntimeException());
         LOGGER.atInfo().addKeyValue("key", "value").log("Key Value Pairs");
+        try (MDC.MDCCloseable ignore = MDC.putCloseable("key", "value")) {
+            LOGGER.atInfo().log("MDC");
+        }
     }
 }
